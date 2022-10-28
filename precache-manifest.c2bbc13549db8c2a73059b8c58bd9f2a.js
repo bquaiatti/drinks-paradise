02faa748d1493dd93a72b38a22cbdea706093b14
bquaiatti <quaiatti@gmail.com>
2022-10-28 10:06:39 +0100

    Updates

---
+++ precache-manifest.c2bbc13549db8c2a73059b8c58bd9f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d529c6e991405c04188b9bd83ce2a4",
+    "url": "/drinks-paradise/index.html"
+  },
+  {
+    "revision": "d0b5315c5c8ec27e7191",
+    "url": "/drinks-paradise/static/css/main.2154b56e.chunk.css"
+  },
+  {
+    "revision": "3c69f37b2f426508b8f1",
+    "url": "/drinks-paradise/static/js/2.98b1616e.chunk.js"
+  },
+  {
+    "revision": "0764d34f6e6ccf27f130be340419d558",
+    "url": "/drinks-paradise/static/js/2.98b1616e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b5315c5c8ec27e7191",
+    "url": "/drinks-paradise/static/js/main.20336d39.chunk.js"
+  },
+  {
+    "revision": "eae9d0e08ca982fc5bc6",
+    "url": "/drinks-paradise/static/js/runtime-main.c423a553.js"
+  }
+]);
